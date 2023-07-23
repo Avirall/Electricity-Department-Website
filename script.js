46a6d238d5b1ab7341ca4de67e75bd7cb813ac4b
Aviral Singh <79758057+Avirall@users.noreply.github.com>
2023-07-24 02:12:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+        function more(){
+        document.getElementById('info-states-more').style="display:block";
+        document.getElementById('info-states-less').style="display:none";
+        document.getElementById('btnLess').style="display:block";
+        document.getElementById('btnCont').style="display:none";
+        document.getElementById('other-container').style="margin-bottom: 10px;";
+        function less(){
+        document.getElementById('info-states-more').style="display:none";
+        document.getElementById('info-states-less').style="display:block";
+        document.getElementById('btnLess').style="display:none";
+        document.getElementById('btnCont').style="display:block";
+        document.getElementById('other-container').style="margin-bottom: 120px;";
+        }
+        }
+        
+        function clickAlert() {
+        alert("Your Credentials Have Been Saved");
+}
